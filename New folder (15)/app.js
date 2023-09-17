8c0d30a615933d838ad86a94294f3b1a32b46764
shahzgithub <143381478+shahzgithub@users.noreply.github.com>
2023-09-18 02:30:12 +0500

    Add files via upload

---
+++ New folder (15)/app.js
@@ -0,0 +1,92 @@
+//chapter 17-20
+//Q1
+const emptyArray = [];
+
+//Q2
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+//Q3
+for (i in range(1, 11));
+    console.log(i);
+
+//Q4
+num = int(prompt("Enter a number"));
+length = int(prompt("Enter the length of the multiplication table"));
+alert("Multiplication table for {num} up to {length} terms:");
+for (i in range(10 + length));
+    result = num * i
+    alert("{num} x {i} = {result}");
+
+//Q5
+fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+print("Fruits in the array:")
+for (fruit in fruits);
+    alert(fruit)
+
+//Q6
+//In html page
+
+//Q7
+
+A = ["cake", "apple pie", "cookie", "chips", "patties"]
+search_item = prompt("Enter an item to search for: ")
+found = False
+for (item in A);
+    if (item.lower() == search_item.lower());
+        found = True
+if (console.log("{search_item} is found in the list.")) {
+} else {
+   (console.log("{search_item} is not found in the list."))
+}
+
+//Q8
+A = [24, 53, 78, 91, 12]
+
+max_num = A[0]
+
+for (num in A);
+    if (num > max_num);
+        max_num = num
+alert("The largest number in the array is: {max_num}")
+
+//Q9
+A = [24, 53, 78, 91, 12]
+
+min_num = A[0]
+
+for (num in A);
+    if (num < min_num);
+        min_num = num
+alert("The smallest number in the array is: {min_num}")
+
+//Q10
+for (i in range(1, 101));
+    if (i % 5 == 0);
+        print(i)
+        5
+        10
+        15
+        20
+        25
+        30
+        35
+        40
+        45
+        50
+        55
+        60
+        65
+        70
+        75
+        80
+        85
+        90
+        95
+        100
+        
